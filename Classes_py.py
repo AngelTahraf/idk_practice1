87afb81ee393614d4d7e5f5b52b5758ceb550d3c
AngelTahraf <111313800+AngelTahraf@users.noreply.github.com>
2022-08-15 16:24:08 -0500

    Update Classes_py.py

--- Classes_py.py
+++ Classes_py.py
@@ -1,1 +1,13 @@
-print("Michd")
+from abc import ABC, abstractmethod
+class persona_info (ABC):
+  @abstractmethod
+  def persona_info_name(self):
+    pass
+  
+class persona_name (persona_info):
+  def persona_data(self,name):
+    self.name = name
+    print("", self.name)
+    
+akira = persona_name
+akira.persona_data(self, "Akira kurusu")
